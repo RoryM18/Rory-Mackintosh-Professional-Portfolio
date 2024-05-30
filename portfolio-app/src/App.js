0a74137b515cff5f2c581cff654ae29da4121337
RoryM18 <40492068@live.napier.ac.uk>
2024-05-30 19:45:33 +0100

    Learned the fundamentals of props and the use of css in react

--- portfolio-app/src/App.js
+++ portfolio-app/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import HelloWorld from "./components/HelloWorld";
+
+import './App.css'
+import Sample from "./components/Sample";
 
 function App() {
+const saygoodbye = "See You Again"
+const number = 15;
+const num = 18;
+const name = "Rory";
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HelloWorld numberprops={num} name={name}/>
+      <div>{saygoodbye} {number + number}</div>
+      <div className="sample-css">Hello App CSS</div>
+
+      <Sample></Sample>
+
     </div>
   );
 }
---
+++ portfolio-app/src/components/Sample.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Style from './Sample.module.css'
+
+function Sample() {
+  return (
+    <div className={Style.myStyle}>Sample</div>
+  )
+}
+
+export default Sample
