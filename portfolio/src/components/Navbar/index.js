311847404d6c6a81dbbabb9579abd05423d1c979
RoryM18 <40492068@live.napier.ac.uk>
2024-06-05 23:35:27 +0100

    Added Navbar Links And Mobile Functionality

--- portfolio/src/components/Navbar/index.js
+++ portfolio/src/components/Navbar/index.js
@@ -1,5 +1,14 @@
 import React from "react";
-import { Nav, NavbarContainer, NavLogo } from "./NavbarElements";
+import { FaBars } from "react-icons/fa";
+import {
+  Nav,
+  NavbarContainer,
+  NavLogo,
+  MobileIcon,
+  NavMenu,
+  NavItem,
+  NavLinks,
+} from "./NavbarElements";
 
 const Navbar = () => {
   return (
@@ -7,6 +16,20 @@ const Navbar = () => {
       <Nav>
         <NavbarContainer>
           <NavLogo to="/">Rory Mackintosh</NavLogo>
+          <MobileIcon>
+            <FaBars />
+          </MobileIcon>
+          <NavMenu>
+            <NavItem>
+              <NavLinks to="about">About</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="about">Portfolio</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="about">Contact</NavLinks>
+            </NavItem>
+          </NavMenu>
         </NavbarContainer>
       </Nav>
     </>
