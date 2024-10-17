789153c411ac5ea6370257f0c28f46ba28efd643
RoryM18 <rorymac126@gmail.com>
2024-10-17 03:03:38 +0100

    Working on footer

--- portfolio/src/components/Footer/index.js
+++ portfolio/src/components/Footer/index.js
@@ -28,54 +28,66 @@ const Footer = () => {
         <FooterLinksContainer>
           <FooterLinksWrapper>
             <FooterLinkItems>
-              <FooterLinkTitle> Aboout Us </FooterLinkTitle>
-              <FooterLink to="/about">Test 1</FooterLink>
-              <FooterLink to="/about">Test 2</FooterLink>
-              <FooterLink to="/about">Test 3</FooterLink>
-              <FooterLink to="/about">Test 4</FooterLink>
+              <FooterLinkTitle> About </FooterLinkTitle>
+              <FooterLink to="/">Home</FooterLink>
+              <FooterLink to="about">About</FooterLink>
+              <FooterLink to="portfolio">Portfolio</FooterLink>
+              <FooterLink to="contact">Contact</FooterLink>
             </FooterLinkItems>
             <FooterLinkItems>
-              <FooterLinkTitle> Aboout Us </FooterLinkTitle>
-              <FooterLink to="/about">Test 1</FooterLink>
-              <FooterLink to="/about">Test 2</FooterLink>
-              <FooterLink to="/about">Test 3</FooterLink>
-              <FooterLink to="/about">Test 4</FooterLink>
+              <FooterLinkTitle> Contact </FooterLinkTitle>
+              <FooterLink to="about">Test 2</FooterLink>
+              <FooterLink to="about">Test 1</FooterLink>
+              <FooterLink to="about">Test 3</FooterLink>
+              <FooterLink to="about">Test 4</FooterLink>
             </FooterLinkItems>
           </FooterLinksWrapper>
           <FooterLinksWrapper>
             <FooterLinkItems>
-              <FooterLinkTitle> Aboout Us </FooterLinkTitle>
-              <FooterLink to="/about">Test 1</FooterLink>
-              <FooterLink to="/about">Test 2</FooterLink>
-              <FooterLink to="/about">Test 3</FooterLink>
-              <FooterLink to="/about">Test 4</FooterLink>
+              <FooterLinkTitle> Top Projects </FooterLinkTitle>
+              <FooterLink to="about">Test 1</FooterLink>
+              <FooterLink to="about">Test 2</FooterLink>
+              <FooterLink to="about">Test 3</FooterLink>
+              <FooterLink to="about">Test 4</FooterLink>
             </FooterLinkItems>
             <FooterLinkItems>
-              <FooterLinkTitle> Aboout Us </FooterLinkTitle>
-              <FooterLink to="/about">Test 1</FooterLink>
-              <FooterLink to="/about">Test 2</FooterLink>
-              <FooterLink to="/about">Test 3</FooterLink>
-              <FooterLink to="/about">Test 4</FooterLink>
+              <FooterLinkTitle> Social Media </FooterLinkTitle>
+              <FooterLink to="Facebook">Test 1</FooterLink>
+              <FooterLink to="Instagram">Test 2</FooterLink>
+              <FooterLink to="Linkedin">Test 3</FooterLink>
+              <FooterLink to="about">Test 4</FooterLink>
             </FooterLinkItems>
           </FooterLinksWrapper>
         </FooterLinksContainer>
         <SocialMedia>
           <SocialMediaWrap>
             <SocialLogo to="/" onClick={toggleHome}>
-              Socials
+              Rory Mackintosh
             </SocialLogo>
             <WebsiteRights>
               Rory Mackintosh Portfolio © {new Date().getFullYear()} All rights
               reserved.
             </WebsiteRights>
             <SocialIcons>
-              <SocialIconsLink href="/" target="_blank" aria-label="Facebook">
+              <SocialIconsLink
+                href="https://www.facebook.com/rory.mackintosh.739"
+                target="_blank"
+                aria-label="Facebook"
+              >
                 <FaFacebook />
               </SocialIconsLink>
-              <SocialIconsLink href="/" target="_blank" aria-label="Instagram">
+              <SocialIconsLink
+                href="https://www.instagram.com/rorymackinto/"
+                target="_blank"
+                aria-label="Instagram"
+              >
                 <FaInstagram />
               </SocialIconsLink>
-              <SocialIconsLink href="/" target="_blank" aria-label="Linkedin">
+              <SocialIconsLink
+                href="https://www.linkedin.com/in/rory-mackintosh-2984632a6/"
+                target="_blank"
+                aria-label="Linkedin"
+              >
                 <FaLinkedin />
               </SocialIconsLink>
             </SocialIcons>
